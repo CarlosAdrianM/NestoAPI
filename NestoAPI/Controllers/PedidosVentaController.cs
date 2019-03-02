90eafb2595952b4baf1b0f858ae652dc2c488920
Carlos Adrián Martínez <carlos_adrian_m@hotmail.com>
2019-03-02 11:56:15 +0100

    PedidoVenta corregido bug con el visto bueno en almacén tienda

--- NestoAPI/Controllers/PedidosVentaController.cs
+++ NestoAPI/Controllers/PedidosVentaController.cs
@@ -656,6 +656,10 @@ namespace NestoAPI.Controllers
                     linea.vistoBueno = true;
                     linea.fechaEntrega = DateTime.Today;
                 }
+                if (!linea.vistoBueno && linea.almacen == Constantes.Productos.ALMACEN_TIENDA)
+                {
+                    linea.vistoBueno = true;
+                }
                 linPedido = crearLineaVta(linea, pedido.numero, pedido.empresa, pedido.iva, plazoPago, pedido.cliente, pedido.contacto, pedido.ruta);
                 linea.baseImponible = linPedido.Base_Imponible;
                 linea.total = linPedido.Total;
@@ -685,6 +689,7 @@ namespace NestoAPI.Controllers
                         texto = "Portes Glovo",
                         precio = respuestaMaps.Coste,
                         iva = pedido.iva,
+                        vistoBueno = true,
                         usuario = pedido.LineasPedido.FirstOrDefault().usuario
                     };
                     linPedido = crearLineaVta(lineaPortes, pedido.numero, pedido.empresa, pedido.iva, plazoPago, pedido.cliente, pedido.contacto, pedido.ruta);
