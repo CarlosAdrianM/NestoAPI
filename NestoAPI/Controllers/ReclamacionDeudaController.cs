699666c904e845977ebe308d3a71a5ead16724f9
Carlos Adrián Martínez <carlos_adrian_m@hotmail.com>
2020-11-05 18:50:18 +0100

    Reclamacion deuda primera version

---
+++ NestoAPI/Controllers/ReclamacionDeudaController.cs
@@ -0,0 +1,47 @@
+﻿using NestoAPI.Infraestructure;
+using NestoAPI.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Http;
+using System.Web.Http.Description;
+
+namespace NestoAPI.Controllers
+{
+    public class ReclamacionDeudaController : ApiController
+    {
+        // GET api/<controller>
+        public IEnumerable<ReclamacionDeuda> Get()
+        {
+            throw new NotImplementedException();
+        }
+
+        // GET api/<controller>/5
+        public string Get(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        // POST api/<controller>
+        [ResponseType(typeof(ReclamacionDeuda))]
+        public async Task<IHttpActionResult> Post([FromBody] ReclamacionDeuda reclamacion)
+        {
+            ServicioReclamacionDeuda servicio = new ServicioReclamacionDeuda();
+            ReclamacionDeuda respuesta = await servicio.ProcesarReclamacionDeuda(reclamacion).ConfigureAwait(false);
+            return Ok(respuesta);
+        }
+
+        // PUT api/<controller>/5
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<controller>/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ NestoAPI/Models/ReclamacionDeuda.cs
@@ -0,0 +1,17 @@
+﻿namespace NestoAPI.Models
+{
+    public class ReclamacionDeuda
+    {
+        public string Correo { get; set; }
+        public string Movil { get; set; }
+        public decimal Importe { get; set; }
+        public string Asunto { get; set; }
+        public string Nombre { get; set; }
+        public string Direccion { get; set; }
+        public string TextoSMS { get; set; }
+
+
+        public bool TramitadoOK { get; set; }
+        public string Enlace { get; set; }
+    }
+}
